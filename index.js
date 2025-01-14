d9d4e2ebdff2f8aec3193b5cc944cdf43b8c5bc8
Manuel Zavala <manuelzavala@its-my-life.local>
2025-01-14 06:25:48 -0800

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-``
+function getFroyoFlavors() {
+  let flavorsInput = prompt("Enter a list of froyo flavors (comma-separated):");
+
+  if (flavorsInput) {
+    let flavorsArray = flavorsInput.split(",").map(flavor => flavor.trim());
+
+    let flavorCount = {};
+    for (let flavor of flavorsArray) {
+      if (flavorCount[flavor]) {
+        flavorCount[flavor]++;
+      } else {
+        flavorCount[flavor] = 1;
+      }
+    }
+    console.table(flavorCount);
+  } else {
+    console.log("No flavors were entered.");
+  }
+}
